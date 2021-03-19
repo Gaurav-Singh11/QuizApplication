8483f59ab61ebf638f2af3b37f11eff1537211c4
Gaurav-Singh11 <gauravanilkumarsingh@gmail.com>
2021-03-19 05:33:32 +0530

    Awesome Quiz Application

---
+++ js/ques.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which statement is true about java?",
+    answer: "Platform independent programming language",
+    options: [
+      "Platform independent programming language",
+      "Platform dependent programming language",
+      "Code dependent programming language",
+      "Sequence dependent programming language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which component is responsible to run java program?",
+    answer: "JRE",
+    options: [
+      "JVM",
+      "JDK",
+      "JRE",
+      "JIT"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which component is responsible for converting bytecode into machine specific code?",
+    answer: "JVM",
+    options: [
+      "JVM",
+      "JDK",
+      "JRE",
+      "JIT"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which component is used to compile, debug and execute java program?",
+    answer: "JDK",
+    options: [
+      "JVM",
+      "JDK",
+      "JRE",
+      "JIT"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the extension of java code files?",
+    answer: ".java",
+    options: [
+      ".java",
+      ".class",
+      ".js",
+      ".py"
+    ]
+  },
+];
